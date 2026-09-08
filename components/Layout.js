95d495664300b2f5c279b977eddee4c394cb12c5
Apriori-Akshara <106730358+Apriori-Akshara@users.noreply.github.com>
2026-09-08 10:37:15 +0530

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,20 @@
 import Footer from './Home/footer'
- 
+import Navbar from './NavbarJS'
+import { useRouter } from 'next/router'
+
 export default function Layout({ children }) {
+  const router = useRouter()
+
+  const isSatExperience =
+    router.pathname === '/SATMocks' ||
+    router.pathname.startsWith('/SATMocks/')
+
   return (
     <>
+      {isSatExperience && <Navbar />}
+
       <main className='top'>{children}</main>
+
       <Footer />
     </>
   )
