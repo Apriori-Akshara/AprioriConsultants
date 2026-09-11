ba679cb18959cc6a6fcde3a8774f41165fe3b20b
Apriori-Akshara <106730358+Apriori-Akshara@users.noreply.github.com>
2026-09-11 20:27:38 +0530

    Improve hero image loading placeholder

--- components/Home/Heroslider.js
+++ components/Home/Heroslider.js
@@ -41,6 +41,7 @@ export default function Heroslider() {
         </Reveal>
         </div>
         <div className={styles.tint1}></div>
+        <div className={styles.heroPlaceholder}></div>
         <Image className={styles.heroimg} src={"/Heroslider/carasoul1.png"} width={1200} height={1200} sizes="100vw" quality={60} fetchPriority="high" alt="hero-image"/>
       </div>
       <div className={styles.heroimgcont}>
