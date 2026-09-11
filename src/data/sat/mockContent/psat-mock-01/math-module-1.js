88368ce7d9f3f805065b454696466cc732e3576e
Apriori-Akshara <106730358+Apriori-Akshara@users.noreply.github.com>
2026-09-11 14:23:28 +0530

    Stage 1: Connect PSAT Mock 01 Math Module 1

---
+++ src/data/sat/mockContent/psat-mock-01/math-module-1.js
@@ -0,0 +1,3 @@
+import { buildMock } from "../goldMockContent";
+export const PSAT_MOCK_01_MATH_MODULE_1 = buildMock({ testId: "psat-mock-01", variant: "psat-nmsqt", section: "math", module: "math-module-1" });
+export default PSAT_MOCK_01_MATH_MODULE_1;
