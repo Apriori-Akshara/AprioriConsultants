d9dbc1decb580592f05460c735008fe761d60fa1
Apriori-Akshara <106730358+Apriori-Akshara@users.noreply.github.com>
2026-09-11 16:46:11 +0530

    Route Stage 1 mocks through production content bank

--- src/data/sat/mockContent/index.js
+++ src/data/sat/mockContent/index.js
@@ -1,40 +1,10 @@
-import PSAT_RW1 from "./psat-mock-01/rw-module-1";
-import PSAT_RW2 from "./psat-mock-01/rw-module-2";
-import PSAT_MATH1 from "./psat-mock-01/math-module-1";
-import PSAT_MATH2 from "./psat-mock-01/math-module-2";
-import SAT_RW1 from "./sat-mock-01/rw-module-1";
-import SAT_RW2 from "./sat-mock-01/rw-module-2";
-import SAT_MATH1 from "./sat-mock-01/math-module-1";
-import SAT_MATH2 from "./sat-mock-01/math-module-2";
+import { PSAT_MOCK_01_CONTENT, SAT_MOCK_01_CONTENT } from "./productionStage1MockContent";
 import { validateMockContent, validateMockPair } from "./mockContentQualityGate";
-
-const PSAT_READING_WRITING = [...PSAT_RW1, ...PSAT_RW2];
-const PSAT_MATH = [...PSAT_MATH1, ...PSAT_MATH2];
-const SAT_READING_WRITING = [...SAT_RW1, ...SAT_RW2];
-const SAT_MATH = [...SAT_MATH1, ...SAT_MATH2];
-
-export const PSAT_MOCK_01_CONTENT = {
-  testId: "psat-mock-01",
-  assessmentVariant: "psat-nmsqt",
-  questionCount: 98,
-  bankQuestionCount: 196,
-  readingWriting: PSAT_READING_WRITING,
-  math: PSAT_MATH,
-};
-
-export const SAT_MOCK_01_CONTENT = {
-  testId: "sat-mock-01",
-  assessmentVariant: "sat-series-a",
-  questionCount: 98,
-  bankQuestionCount: 196,
-  readingWriting: SAT_READING_WRITING,
-  math: SAT_MATH,
-};
 
 validateMockContent(PSAT_MOCK_01_CONTENT);
 validateMockContent(SAT_MOCK_01_CONTENT);
 validateMockPair(PSAT_MOCK_01_CONTENT, SAT_MOCK_01_CONTENT);
 
+export { PSAT_MOCK_01_CONTENT, SAT_MOCK_01_CONTENT };
 export const SAT_PSAT_STAGE_1_MOCKS = [PSAT_MOCK_01_CONTENT, SAT_MOCK_01_CONTENT];
-
 export default SAT_PSAT_STAGE_1_MOCKS;
