32dc8f6ed4e9a1ab8ab06bb71ba3db634083001c
Apriori-Akshara <106730358+Apriori-Akshara@users.noreply.github.com>
2026-09-11 14:23:56 +0530

    Stage 1: Connect SAT Mock 01 R&W Module 2

---
+++ src/data/sat/mockContent/sat-mock-01/rw-module-2.js
@@ -0,0 +1,3 @@
+import { buildMock } from "../goldMockContent";
+export const SAT_MOCK_01_RW_MODULE_2 = buildMock({ testId: "sat-mock-01", variant: "sat-series-a", section: "reading-writing", module: "rw-module-2" });
+export default SAT_MOCK_01_RW_MODULE_2;
