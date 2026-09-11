6ed8b186431cfd11848b95025023a0b2234c8b72
Apriori-Akshara <106730358+Apriori-Akshara@users.noreply.github.com>
2026-09-11 14:23:14 +0530

    Stage 1: Connect PSAT Mock 01 R&W Module 1

--- src/data/sat/mockContent/psat-mock-01/rw-module-1.js
+++ src/data/sat/mockContent/psat-mock-01/rw-module-1.js
@@ -1,4 +1,10 @@
-// Original Apriori Consultants content; fully original and independently authored.
-export const PSAT_MOCK_01_RW_MODULE_1 = " + "[]" + ";
+import { buildMock } from "../goldMockContent";
+
+export const PSAT_MOCK_01_RW_MODULE_1 = buildMock({
+  testId: "psat-mock-01",
+  variant: "psat-nmsqt",
+  section: "reading-writing",
+  module: "rw-module-1",
+});
 
 export default PSAT_MOCK_01_RW_MODULE_1;
