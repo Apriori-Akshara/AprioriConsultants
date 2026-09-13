8808602fdb707053edd3870dea88f0a3f519fc2c
Apriori-Akshara <106730358+Apriori-Akshara@users.noreply.github.com>
2026-09-13 13:38:25 +0530

    Batch M: Add SAT Series B to navigation

--- components/NavbarJS.js
+++ components/NavbarJS.js
@@ -62,6 +62,7 @@ export default function navbar() {
           <Link href='/' className={isActive('/') ? `${styles.active}`: `${styles.link}`}>Home</Link>
           <Link href='/Admissions' className={isActive('/Admissions') ? `${styles.active}`: `${styles.link}`}>Admissions</Link>
           <Link href='/TestPrep' className={isActive('/TestPrep') ? `${styles.active}`: `${styles.link}`}>Test Prep</Link>
+          <Link href='/SATMocksSeriesB' className={isActive('/SATMocksSeriesB') ? `${styles.active}`: `${styles.link}`}>SAT Series B</Link>
           <Link href='/Languages' className={isActive('/Languages') ? `${styles.active}`: `${styles.link}`}>Languages</Link>
           <Link href='/Testimonials' className={isActive('/Testimonials') ? `${styles.active}`: `${styles.link}`}>Testimonials</Link>
           <Link href='/About' className={isActive('/About') ? `${styles.active}`: `${styles.link}`}>About Us</Link>
@@ -113,7 +114,7 @@ export default function navbar() {
             <svg width="50" height="50" viewBox="0 0 100 100">
                 <path className={`${styles.line} ${styles.line1}`} d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058" />
                 <path className={`${styles.line} ${styles.line2}`} d="M 20,50 H 80" />
-                <path className={`${styles.line} ${styles.line3}`} d="M 20,70.999954 H 80.000231 C 80.000231,70.999954 94.498839,71.182648 94.532987,33.288669 94.543142,22.019327 90.966081,18.329754 85.259173,18.331003 79.552261,18.332249 75.000211,25.000058 75.000211,25.000058 L 25.000021,74.999942" />
+                <path className={`${styles.line} ${styles.line3}`} d="M 20,70.999954 H 80.000231 C 80.000231,70.999954 94.498839,71.182648 94.532987,33.288669 94.543142,22.019327 90.966081,18.329754 85.259173,18.331003 79.552261,18.332249 75.000211,25.000058 25.000021,74.999942" />
               </svg>
           </button>
     </div>
