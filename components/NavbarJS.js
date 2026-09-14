a3965a09f5faebf17af9c78e0991990af0e0fe7b
Apriori-Akshara <106730358+Apriori-Akshara@users.noreply.github.com>
2026-09-14 11:03:49 +0530

    fix: remove SAT Series B from global navbar

--- components/NavbarJS.js
+++ components/NavbarJS.js
@@ -22,104 +22,91 @@ export default function navbar() {
 
   const controlNavbar = () => {
     if (typeof window !== 'undefined') { 
-      if (window.scrollY > lastScrollY && window.scrollY >= 150) { // if scroll down hide the navbar
+      if (window.scrollY > lastScrollY && window.scrollY >= 150) {
         setShow(false); 
-      } else { // if scroll up show the navbar
+      } else {
         setShow(true);  
       }
-      // remember current page location to use in the next move
       setLastScrollY(window.scrollY); 
     }
   };
 
-     const toggleDropdown = () => {
+  const toggleDropdown = () => {
     setDropdownVisible(!isDropdownVisible);
   };
 
-    const handleLogout = () => {
+  const handleLogout = () => {
     window.location.href = '/';
-    dispatch(logout()); // Dispatch logout action
-    setDropdownVisible(false); // Hide dropdown after logging out
+    dispatch(logout());
+    setDropdownVisible(false);
   };
 
   useEffect(() => {   
     if (typeof window !== 'undefined') {
       window.addEventListener('scroll', controlNavbar);
-      // cleanup function
       return () => {
         window.removeEventListener('scroll', controlNavbar);
       };
     }  
-
   }, [lastScrollY]);
 
   return (
     <div className={show ? `${styles.show}` : `${styles.hide}`}>
-    <div className={styles.navbar}>
+      <div className={styles.navbar}>
         <Image className={styles.logoimage} src={'/logo/logo.jpg'} width={400} height={400} alt="logo-image"/>
-      <div>
-        <ul className={toggle ? `${styles.uldown}` : `${styles.ul}`}>
-          <Link href='/' className={isActive('/') ? `${styles.active}`: `${styles.link}`}>Home</Link>
-          <Link href='/Admissions' className={isActive('/Admissions') ? `${styles.active}`: `${styles.link}`}>Admissions</Link>
-          <Link href='/TestPrep' className={isActive('/TestPrep') ? `${styles.active}`: `${styles.link}`}>Test Prep</Link>
-          <Link href='/SATMocksSeriesB' className={isActive('/SATMocksSeriesB') ? `${styles.active}`: `${styles.link}`}>SAT Series B</Link>
-          <Link href='/Languages' className={isActive('/Languages') ? `${styles.active}`: `${styles.link}`}>Languages</Link>
-          <Link href='/Testimonials' className={isActive('/Testimonials') ? `${styles.active}`: `${styles.link}`}>Testimonials</Link>
-          <Link href='/About' className={isActive('/About') ? `${styles.active}`: `${styles.link}`}>About Us</Link>
-          <Link href='/Contact' className={isActive('/Contact') ? `${styles.active}`: `${styles.link}`}>Contact Us</Link>
-        </ul>
-      </div>
+        <div>
+          <ul className={toggle ? `${styles.uldown}` : `${styles.ul}`}>
+            <Link href='/' className={isActive('/') ? `${styles.active}`: `${styles.link}`}>Home</Link>
+            <Link href='/Admissions' className={isActive('/Admissions') ? `${styles.active}`: `${styles.link}`}>Admissions</Link>
+            <Link href='/TestPrep' className={isActive('/TestPrep') ? `${styles.active}`: `${styles.link}`}>Test Prep</Link>
+            <Link href='/Languages' className={isActive('/Languages') ? `${styles.active}`: `${styles.link}`}>Languages</Link>
+            <Link href='/Testimonials' className={isActive('/Testimonials') ? `${styles.active}`: `${styles.link}`}>Testimonials</Link>
+            <Link href='/About' className={isActive('/About') ? `${styles.active}`: `${styles.link}`}>About Us</Link>
+            <Link href='/Contact' className={isActive('/Contact') ? `${styles.active}`: `${styles.link}`}>Contact Us</Link>
+          </ul>
+        </div>
 
-      <div className={styles.btnsconta}>
-      <Link href='/Courses'><button className={styles.loginBtn}>Courses</button></Link>
-              <div className={styles.endflex}>
-        {user ? (
-          <div className={styles.userInfoContainer}>
-            <div className={styles.userinfocont}>
-            <div className={styles.userInfo} onClick={toggleDropdown}>
-              <FaUserCircle className={styles.userIcon} />
-              <span className={styles.userIdm}>{user?.name?.slice(0,8)}{user?.name?.length > 14 ? '...' : ''}</span>
-            </div>
-            </div>
-            {/* Dropdown menu */}
-            {isDropdownVisible && (
-              <div className={styles.dropdownMenu}>
-                <Link href={'/Profile'} className={styles.logoutButton} onClick={toggleDropdown}>Profile</Link>
-                {/* <Link href={'/User'} className={styles.logoutButton} onClick={toggleDropdown}>Courses</Link>
-                <Link href={'/Forum'} className={styles.logoutButton} onClick={toggleDropdown}>Forum</Link> */}
-                <button className={styles.logoutButton} onClick={handleLogout}>Logout</button>
+        <div className={styles.btnsconta}>
+          <Link href='/Courses'><button className={styles.loginBtn}>Courses</button></Link>
+          <div className={styles.endflex}>
+            {user ? (
+              <div className={styles.userInfoContainer}>
+                <div className={styles.userinfocont}>
+                  <div className={styles.userInfo} onClick={toggleDropdown}>
+                    <FaUserCircle className={styles.userIcon} />
+                    <span className={styles.userIdm}>{user?.name?.slice(0,8)}{user?.name?.length > 14 ? '...' : ''}</span>
+                  </div>
+                </div>
+                {isDropdownVisible && (
+                  <div className={styles.dropdownMenu}>
+                    <Link href={'/Profile'} className={styles.logoutButton} onClick={toggleDropdown}>Profile</Link>
+                    <button className={styles.logoutButton} onClick={handleLogout}>Logout</button>
+                  </div>
+                )}
+              </div>
+            ) : (
+              <div style={{ display: "flex", gap: "8px" }}>
+                <Link href="/Auth">
+                  <button className={styles.loginBtn}>Login</button>
+                </Link>
+                <Link href="/Register">
+                  <button className={styles.loginBtn}>Register</button>
+                </Link>
               </div>
             )}
           </div>
-        ) : (
-          <div style={{ display: "flex", gap: "8px" }}>
-  <Link href="/Auth">
-    <button className={styles.loginBtn}>
-      Login
-    </button>
-  </Link>
-
-  <Link href="/Register">
-    <button className={styles.loginBtn}>
-      Register
-    </button>
-  </Link>
-</div>
-        )}
         </div>
-      </div>
 
-      <div className={styles.btnscontainer}>
+        <div className={styles.btnscontainer}>
           <button onClick={()=>{setToggle(!toggle)}} className={toggle ? `${styles.menu} ${styles.opened}` : `${styles.menu}`} aria-label="Main Menu">
             <svg width="50" height="50" viewBox="0 0 100 100">
-                <path className={`${styles.line} ${styles.line1}`} d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058" />
-                <path className={`${styles.line} ${styles.line2}`} d="M 20,50 H 80" />
-                <path className={`${styles.line} ${styles.line3}`} d="M 20,70.999954 H 80.000231 C 80.000231,70.999954 94.498839,71.182648 94.532987,33.288669 94.543142,22.019327 90.966081,18.329754 85.259173,18.331003 79.552261,18.332249 75.000211,25.000058 75.000211,25.000058 L 25.000021,74.999942" />
-              </svg>
+              <path className={`${styles.line} ${styles.line1}`} d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058" />
+              <path className={`${styles.line} ${styles.line2}`} d="M 20,50 H 80" />
+              <path className={`${styles.line} ${styles.line3}`} d="M 20,70.999954 H 80.000231 C 80.000231,70.999954 94.498839,71.182648 94.532987,33.288669 94.543142,22.019327 90.966081,18.329754 85.259173,18.331003 79.552261,18.332249 75.000211,25.000058 75.000211,25.000058 L 25.000021,74.999942" />
+            </svg>
           </button>
-    </div>
-
-    </div>
+        </div>
+      </div>
     </div>
   )
 }
